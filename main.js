081b337dcf8e04f6f5f5f60cebd3982349453e35
AmyHannahMorgan <47041871+AmyHannahMorgan@users.noreply.github.com>
2020-01-16 11:25:54 +0000

    + directions to PathSegment class and assignment

--- main.js
+++ main.js
@@ -7,6 +7,46 @@ class PathSegment {
       this.abY = this.y * width;
       this.centerX = this.abX + (width / 2);
       this.centerY = this.abY + (width / 2);
+
+      this.n = null
+      this.s = null
+      this.e = null
+      this.w = null
+      this.nw = null
+      this.ne = null
+      this.sw = null
+      this.se = null
+    }
+
+    setDirection(direction, value) {
+        switch(direction) {
+            case 'n':
+                this.n = value;
+                break;
+            case 's':
+                this.s = value;
+                break;
+            case 'e':
+                this.e = value;
+                break;
+            case 'w':
+                this.w = value;
+                break;
+            case 'nw':
+                this.nw = value;
+                break;
+            case 'ne':
+                this.ne = value;
+                break;
+            case 'sw':
+                this.sw = value;
+                break;
+            case 'se':
+                this.se = value;
+                break;
+            default:
+                throw `ValueError: ${direction} is not a valid direction`;
+        }
     }
   }
   
@@ -25,6 +65,49 @@ class PathSegment {
       ar.push(new PathSegment(x, y, pathWidth));
     }
     pathSegments.push(ar);
+  }
+
+  for(let x = 0; x < pathSegments.length; x++) {
+      for(let y = 0; y < pathSegments[x].length; y++) {
+          let currNode = pathSegments[x][y];
+
+          let topEdge = y === 0 ? true : false
+          let bottomEdge = y === pathSegments[x].length - 1 ? true : false
+          let leftEdge = x === 0 ? true : false
+          let rightEdge = x === pathSegments.length - 1 ? true : false
+
+          if(!topEdge) {
+            currNode.setDirection('n', pathSegments[x][y - 1]);
+          }
+
+          if(!bottomEdge) {
+              currNode.setDirection('s', pathSegments[x][y + 1]);
+          }
+
+          if(!leftEdge) {
+              currNode.setDirection('w', pathSegments[x - 1][y]);
+          }
+
+          if(!rightEdge) {
+              currNode.setDirection('e', pathSegments[x + 1][y]);
+          }
+
+          if(!topEdge && !leftEdge) {
+              currNode.setDirection('nw', pathSegments[x - 1][y - 1]);
+          }
+
+          if(!topEdge && !rightEdge) {
+              currNode.setDirection('ne', pathSegments[x + 1][y - 1]);
+          }
+
+          if(!bottomEdge && !leftEdge) {
+              currNode.setDirection('sw', pathSegments[x - 1][y + 1]);
+          }
+
+          if(!bottomEdge && !rightEdge) {
+              currNode.setDirection('se', pathSegments[x + 1][y + 1]);
+          }
+      }
   }
   
   function buildPaths() {
